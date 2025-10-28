8562c88f20f9374073da2317b2dd66b1acfa2a90
Sankalp Patel <101709238+Sankalp2009@users.noreply.github.com>
2025-10-28 10:51:19 +0530

    client_fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import Header from "./Component/Header.jsx"
-import AllRoutes from "./Component/AllRoutes.jsx"
+import Header from "./Component/Header.jsx";
+import AllRoutes from "./Component/AllRoutes.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
-
   return (
     <div>
-     <Header />
-          <Toaster variant="solid" size="md" />
-     <AllRoutes />
+      <Header />
+      <Toaster variant="solid" size="md" />
+      <AllRoutes />
+       <SpeedInsights />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
